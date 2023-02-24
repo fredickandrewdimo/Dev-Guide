4dd603851b537f9c75e4123f4c8116da80456faf
Andrew <fredickandrewdimo@gmail.com>
2023-02-24 12:33:28 +0800

    Sample Outputing Guides in Home

--- Dev.Guide/src/components/Home.jsx
+++ Dev.Guide/src/components/Home.jsx
@@ -1,13 +1,54 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title:
+        "From Zero to Front End Hero: A Beginner's Guide to Web Development",
+      body: "Lorem ipsum...",
+      author: "Andrew",
+      id: 1,
+    },
+    {
+      title: "A Step-by-Step Guide to Becoming a Front End Developer",
+      body: "Lorem ipsum...",
+      author: "Andrew",
+      id: 2,
+    },
+    {
+      title:
+        "Mastering JavaScript in 2023: A Comprehensive Guide for Beginners",
+      body: "Lorem ipsum...",
+      author: "Andrew",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="flex-col text-center">
       <h1 className="text-white text-6xl font-bold">
         Empower Fellow Developers
       </h1>
-      <p className="text-gray-400 mt-7">Share Your Knowledge with DEV.GUIDE!</p>
+      <p className="text-gray-400 mt-7 text-xl">
+        Share Your Knowledge with DEV.GUIDE!
+      </p>
       <button className="text-white py-3 px-4 bg-green-600 font-bold rounded text-xl mt-8">
         Create Now
       </button>
+      <div className="mt-32">
+        <h1 className="text-4xl font-bold text-white">ALL GUIDES</h1>
+        <div className="guide-container mt-10 text-white md:w-4/5 mx-auto text-left">
+          {blogs.map((blog) => (
+            <div
+              className="guide-preview border-2 my-5 rounded p-5 hover:border-green-600 hover:cursor-pointer"
+              key={blog.id}
+            >
+              <h2 className="font-bold text-xl">{blog.title}</h2>
+              <p className="mt-3 text-lg">Written by {blog.author}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- Dev.Guide/src/components/Navbar.jsx
+++ Dev.Guide/src/components/Navbar.jsx
@@ -5,15 +5,18 @@ const Navbar = () => {
     <div className="flex items-center justify-between mx-auto w-11/12 md:w-9/12 py-5">
       <h1 className="text-white font-bold text-2xl">DEV.GUIDE</h1>
       <div className="text-lg text-white">
-        <a className="mx-2" href="#">
+        <a className="mx-2" href="/">
           Home
         </a>
-        <a className="mx-2" href="#">
+        <a className="mx-2" href="/blogs">
           Blogs
         </a>
-        <button className="mx-2 py-2 px-3 rounded bg-white text-neutral-900 font-bold">
+        <a
+          href="/create"
+          className="mx-2 py-2 px-4 rounded bg-white text-neutral-900 font-bold"
+        >
           Create
-        </button>
+        </a>
       </div>
     </div>
   );
