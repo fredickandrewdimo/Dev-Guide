17847091ce1333e7a7179afcb4e04c27cd4c0f01
Andrew <fredickandrewdimo@gmail.com>
2023-02-25 19:07:19 +0800

    added a reusable custom hook for fetching data

--- Dev.Guide/src/components/Home.jsx
+++ Dev.Guide/src/components/Home.jsx
@@ -1,31 +1,13 @@
 import { useState, useEffect } from "react";
+import useFetch from "../customHooks/useFetch";
 import GuideList from "./GuideList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="flex-col text-center">
