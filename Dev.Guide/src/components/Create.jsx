85dad1ff73aa33f539abcd25d7dd9e9d3c4e8aaa
Andrew <fredickandrewdimo@gmail.com>
2023-02-26 21:14:59 +0800

    create guide feature added and a POST request to the json-server.

--- Dev.Guide/src/components/Create.jsx
+++ Dev.Guide/src/components/Create.jsx
@@ -1,36 +1,73 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added :)");
+      setIsPending(false);
+    });
+  };
+
   return (
     <div className="create text-white md:w-4/5 mx-auto">
       <h1 className="text-3xl md:text-4xl text-white font-bold mb-8 text-center">
         CREATE GUIDE
       </h1>
-      <form className="flex-col">
-        <div className="my-4">
+      <form onSubmit={handleSubmit} className="flex-col">
+        <div className="my-4 text-white">
           <label className="text-xl font-bold">TITLE</label>
           <input
-            className="block w-full rounded h-10 md:h-16 p-2 text-black mt-2"
+            className="block w-full rounded h-10 md:h-16 p-2 mt-2 bg-neutral-900 border-2"
             type="text"
             required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="my-6">
           <label className="text-xl font-bold">BODY</label>
           <textarea
-            className="block block w-full rounded h-40 md:h-60 p-2 text-black mt-2"
+            className="block block w-full rounded h-40 md:h-60 p-2 mt-2 bg-neutral-900 border-2"
             required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
           ></textarea>
         </div>
         <div className="my-4">
           <label className="text-xl font-bold">AUTHOR</label>
           <input
-            className="block w-full rounded h-10 md:h-16 p-2 text-black mt-2"
+            className="block w-full rounded h-10 md:h-16 p-2 mt-2 bg-neutral-900 border-2"
             type="text"
+            required
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
           />
         </div>
         <div className="flex justify-end mt-8">
-          <button className="bg-green-400 text-neutral-900 py-2 px-4 md:py-3 md:px-12 rounded font-bold hover:bg-green-600">
-            SUBMIT
-          </button>
+          {!isPending && (
+            <button className="bg-green-400 text-neutral-900 py-2 px-4 md:py-3 md:px-12 rounded font-bold hover:bg-green-600">
+              SUBMIT
+            </button>
+          )}
+          {isPending && (
+            <button className="bg-green-400 text-neutral-900 py-2 px-4 md:py-3 md:px-12 rounded font-bold hover:bg-green-600">
+              Adding Blog...
+            </button>
+          )}
         </div>
       </form>
     </div>
